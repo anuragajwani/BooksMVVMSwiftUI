c56f8cc6567b7f05003eed10280393079844ddf8
Anurag Ajwani <anurag.ajwani@gmail.com>
2023-03-31 21:36:36 +0100

    Initial Commit

---
+++ BooksMVVMSwiftUI/BooksMVVMSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  BooksMVVMSwiftUIApp.swift
+//  BooksMVVMSwiftUI
+//
+//  Created by Anurag Ajwani on 31/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct BooksMVVMSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
